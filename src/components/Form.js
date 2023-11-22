d390abaadfce8ced10abcf754a4bcc0b2fa3386a
Becca Jonas <beccajonas3@gmail.com>
2023-11-22 10:31:31 -0500

    working

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,22 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(e) {
+    e.preventDefault()
+    setFirstName(e.target.value)
+  }
+
+  function handleLastNameChange(e) {
+    e.preventDefault()
+    setLastName(e.target.value)
+  }
+
+  console.log(firstName, lastName);
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" value={firstName} onChange={handleFirstNameChange}/>
+      <input type="text" value={lastName} onChange={handleLastNameChange} />
       <button type="submit">Submit</button>
     </form>
   );
